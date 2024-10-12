cbe2fa12d6219b577c84dc936b3c1c4ed836a736
Ostafi SASHA <maricelostafi79@gmail.com>
2024-10-12 10:41:50 +0300

    Добавьте файлы проекта.

---
+++ string_new_delete.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void deleteRow(int** matrix, int& rows, int rowIndex) {
+    if (rowIndex >= 0 && rowIndex < rows) {
+        delete[] matrix[rowIndex];
+        for (int i = rowIndex; i < rows - 1; ++i) {
+            matrix[i] = matrix[i + 1];
+        }
+        rows--;
+    }
+    else {
+        cout << "Неправильний індекс рядка" << endl;
+    }
+}
+
+int main() {
+    int rows = 3;
+    int cols = 3;
+    int** matrix = new int* [rows];
+
+    for (int i = 0; i < rows; ++i) {
+        matrix[i] = new int[cols];
+        for (int j = 0; j < cols; ++j) {
+            matrix[i][j] = i * cols + j + 1;
+        }
+    }
+
+    int rowToDelete = 1;
+    deleteRow(matrix, rows, rowToDelete);
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
+}
